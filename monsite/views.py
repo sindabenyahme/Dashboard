9b41d407a58826ae0befc7e39e467264bf634bef
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2024-05-10 14:59:43 +0200

    push9

--- monsite/views.py
+++ monsite/views.py
@@ -73,81 +73,77 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 
-def dash(request):
-    files = File.objects.all()  
-    excel_data = []
-    graph_html = ''  # Définir une valeur par défaut pour éviter l'erreur UnboundLocalError
-
-    for file in files:
-        # Load Excel data into a pandas DataFrame, skipping the first row
-        df = pd.read_excel(file.file, skiprows=1)
-        excel_data.append(df)
-        # Assuming each Excel file contains only one sheet
-        file.file = str(file.file).split('/')[-1] if '/' in str(file.file) else str(file.file).split('\\')[-1]
-        print(file.file)
-        
-    # Concatenate all dataframes into one
-    if excel_data:
-        combined_df = pd.concat(excel_data, ignore_index=True)
-        # Calculate the number of calls for each point of call
-        calls_per_point_of_call = combined_df['Point d\'appel'].value_counts()
-
-        plt.figure(figsize=(12, 6))  # Ajuster la taille de la figure
-        calls_per_point_of_call.plot(kind='bar', color='skyblue', fontsize=12)  # Ajuster la taille de la police
-        plt.title('Nombre d\'appels par point d\'appel', fontsize=16)  # Ajuster la taille du titre
-        plt.xlabel('Point d\'appel', fontsize=14)  # Ajuster la taille de l'étiquette x
-        plt.ylabel('Nombre d\'appels', fontsize=14)  # Ajuster la taille de l'étiquette y
-        plt.xticks(rotation=45, fontsize=12)  # Ajuster la taille de l'étiquette x et la rotation
-        plt.yticks(fontsize=12)  # Ajuster la taille de l'étiquette y
-        plt.tight_layout()
-
-        # Convert the plot to an image
-        buffer = BytesIO()
-        plt.savefig(buffer, format='png')
-        buffer.seek(0)
-        image_png = buffer.getvalue()
-        buffer.close()
-
-        # Convert image to base64 format
-        graph = base64.b64encode(image_png).decode()
-
-        # Embed the image in HTML
-        graph_html = f'<img src="data:image/png;base64,{graph}" style="width:100%">'
-
-        # Convert the combined dataframe to a list of dictionaries
-        table_data = combined_df.to_dict(orient='records')
-    else:
-        table_data = []
-        
-    # Create a graph for calls per day and night
-    def is_night(hour):
-        return hour < 6 or hour >= 20
-
-    combined_df['Date'] = pd.to_datetime(combined_df['Date'], format='mixed', errors='coerce')
-
-    combined_df['Period'] = combined_df['Date'].apply(lambda x: 'Nuit' if is_night(x.hour) else 'Jour')
-    calls_per_period = combined_df.groupby('Period').size()
-
-    plt.figure(figsize=(8, 6))
-    calls_per_period.plot(kind='bar', color=['skyblue', 'salmon'], alpha=0.7)
-    plt.title('Nombre d\'appels par période')
-    plt.xlabel('Période')
-    plt.ylabel('Nombre d\'appels')
-    plt.xticks(rotation=0)
+def ghraphe1(data):
+    plt.figure(figsize=(12, 6))
+    data.plot(kind='bar', color='skyblue', fontsize=12)
+    plt.title('Nombre d\'appels par point d\'appel', fontsize=16)
+    plt.xlabel('Point d\'appel', fontsize=14)
+    plt.ylabel('Nombre d\'appels', fontsize=14)
+    plt.xticks(rotation=45, fontsize=12)
+    plt.yticks(fontsize=12)
     plt.tight_layout()
-    
-    # Save the plot as image
+
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
     image_png = buffer.getvalue()
     buffer.close()
 
-    # Convert image to base64 format
-    graph_calls_period = base64.b64encode(image_png).decode()
+    graph = base64.b64encode(image_png).decode()
+    graph_html = f'<img src="data:image/png;base64,{graph}" style="width:100%">'
 
-    # Embed the image in HTML
-    graph_calls_period_html = f'<img src="data:image/png;base64,{graph_calls_period}" style="width:100%">'
+    return graph_html
+
+def graphe2(data):
+    plt.figure(figsize=(8, 6))
+    data.plot(kind='bar', color=['skyblue', 'salmon'], alpha=0.7)
+    plt.title('Nombre d\'appels par période')
+    plt.xlabel('Période')
+    plt.ylabel('Nombre d\'appels')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    buffer.close()
+
+    graph = base64.b64encode(image_png).decode()
+    graph_html = f'<img src="data:image/png;base64,{graph}" style="width:100%">'
+
+    return graph_html
+
+def dash(request):
+    def is_night(hour):
+        return hour < 6 or hour >= 20
+
+    files = File.objects.all()  
+    excel_data = []
+    graph_html = ''
+    graph_calls_period_html = ''
+
+    for file in files:
+        df = pd.read_excel(file.file, skiprows=1)
+        excel_data.append(df)
+        file.file = str(file.file).split('/')[-1] if '/' in str(file.file) else str(file.file).split('\\')[-1]
+        print(file.file)
+        
+    if excel_data:
+        combined_df = pd.concat(excel_data, ignore_index=True)
+        calls_per_point_of_call = combined_df['Point d\'appel'].value_counts()
+
+        graph_html = ghraphe1(calls_per_point_of_call)
+
+        table_data = combined_df.to_dict(orient='records')
+    else:
+        table_data = []
+
+    combined_df['Date'] = pd.to_datetime(combined_df['Date'], format='mixed', errors='coerce')
+    combined_df['Period'] = combined_df['Date'].apply(lambda x: 'Nuit' if is_night(x.hour) else 'Jour')
+    calls_per_period = combined_df.groupby('Period').size()
+
+    graph_calls_period_html = graphe2(calls_per_period)
 
     return render(request, 'stats.html', {'files': files, 'table_data': table_data, 'graph_html': graph_html, 'graph_calls_period_html': graph_calls_period_html})
 
